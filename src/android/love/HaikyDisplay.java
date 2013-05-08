bc53b94ff7477c99c4f3339e25a6c5c60bda0748
Viktor Chmel <viktor.chmel@gmail.com>
2013-05-08 11:51:20 +0300

    [add] button to hide/show haiku text

--- src/android/love/HaikyDisplay.java
+++ src/android/love/HaikyDisplay.java
@@ -3,6 +3,8 @@ package android.love;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class HaikyDisplay extends Activity {
 
@@ -18,6 +20,14 @@ public class HaikyDisplay extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.haiky_display, menu);
         return true;
+    }
+    
+    public void onLoveButtonClicked(View view) {
+    	TextView textView = (TextView) findViewById(R.id.haikuTextView);
+    	if (textView.getVisibility() == View.INVISIBLE) 
+    			textView.setVisibility(View.VISIBLE);
+    	else
+    			textView.setVisibility(View.INVISIBLE);
     }
     
 }
